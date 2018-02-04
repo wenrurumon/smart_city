222c686637cfe90c6994e15dca7177e8ef8990e9
進撃の雯雯老師 <askahu6@gmail.com>
2018-02-04 12:03:20 +0800

    Create easy_go2.sql

---
+++ test_server/easy_go2.sql
@@ -0,0 +1,6 @@
+create table lc2 as 
+select final_grid_id, ptype, count(1) as count, count(distinct uid) as countuid
+from stay_poi
+where date = 20170901 and city = 'V0110000';
+
+select count(1) from lc2;
