2399dbaf9e9f696d8486362dbb59750798ce55fe
進撃の雯雯老師 <askahu6@gmail.com>
2018-02-04 13:03:53 +0800

    Update easy_go.sql

--- test_server/easy_go.sql
+++ test_server/easy_go.sql
@@ -2,5 +2,6 @@ select * from (
 select final_grid_id, ptype, count(1) as count, count(distinct uid) as countuid
 from stay_poi
 where date = 20170901 and city = 'V0110000'
+group by final_grid_id, ptype
 ) 
 limit 10000;
