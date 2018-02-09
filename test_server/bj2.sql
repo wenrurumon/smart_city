8956f6cd5045fe709cb19aa875a9183dfd419f74
進撃の雯雯老師 <askahu6@gmail.com>
2018-02-09 15:15:41 +0800

    Update bj2.sql

--- test_server/bj2.sql
+++ test_server/bj2.sql
@@ -3,3 +3,7 @@ select
 final_grid_id,ptype,gender_level,age_level,is_local,u_num,w_num,
 cast(row_number() over(partition by 1) / 150000 as int) as index
 from lc1;
+
+################
+
+select index, count(1) as count from lc2 group by index;
