f322a1ce19ec83cc2bbce7c9b60a63619af45e19
進撃の雯雯老師 <askahu6@gmail.com>
2018-02-09 15:47:53 +0800

    Update bj2.sql

--- test_server/bj2.sql
+++ test_server/bj2.sql
@@ -1,9 +1,9 @@
 create table lc2 as 
 select 
 final_grid_id,ptype,gender_level,age_level,is_local,u_num,w_num,
-cast(row_number() over(partition by 1) / 150000 as int) as index
+cast(row_number() over(partition by 1) / 150000 as int) as slides
 from lc1;
 
 ################
 
-select index, count(1) as count from lc2 group by index;
+select index, count(1) as count from lc2 group by slides;
