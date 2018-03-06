08eeca459eab74c2e0902cbf2868088f19ae223f
進撃の雯雯老師 <askahu6@gmail.com>
2018-03-06 16:08:39 +0800

    Create 2_city_coef_setup.R

---
+++ standard_block/2_city_coef_setup.R
@@ -0,0 +1,13 @@
+
+library(geosphere)
+
+options(digits=22)
+x <- '39.588558403090445 115.85861214160138 40.66312427234367 117.2424039947548'
+x <- as.numeric(strsplit(x,' ')[[1]])
+
+xm <- (x[1:2]+x[3:4])/2
+latdist <- distm(c(xm[2],x[1]),c(xm[2],x[3]))
+londist <- distm(c(x[2],xm[1]),c(x[4],xm[1]))
+
+(x[4] - x[2])/londist * 200
+(x[3] - x[1])/latdist * 200
