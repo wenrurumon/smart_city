affe8df2c8b8139624e40525a8d83bef6e3f0189
進撃の雯雯老師 <askahu6@gmail.com>
2018-03-21 16:43:17 +0800

    Update ss_wgs44.sql

--- test_daas/ss_wgs44.sql
+++ test_daas/ss_wgs44.sql
@@ -1,1 +1,2 @@
-select county_code,city_code, city_name, min(ext_min_x) as minx, min(ext_min_y) as miny, avg(ext_max_x - ext_min_x) as avgx, avg(ext_max_y - ext_min_y) as avgy from ss_grid_wgs84 group by county_code,city_code, city_name;
+create table lc_grid_map as
+select city_code, city_name, min(ext_min_x) as minx, min(ext_min_y) as miny, avg(ext_max_x - ext_min_x) as avgx, avg(ext_max_y - ext_min_y) as avgy from ss_grid_wgs84 group by city_code, city_name;
