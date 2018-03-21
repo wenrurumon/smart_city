ce5aa2a4cfed6b0559a14d7251791c9ebcea2233
進撃の雯雯老師 <askahu6@gmail.com>
2018-03-21 14:05:41 +0800

    Create stay_month_tbj.sql

---
+++ test_daas/stay_month_tbj.sql
@@ -0,0 +1,20 @@
+create table sm_tbj1 as
+select f.uid, f.city, f.grid_id, f.ptype, f.date, f.nstime as stime, f.netime as etime
+from (
+select sm.uid, sm.city, sm.grid_id, sm.ptype, sm.date, 
+case when (minstime = stime) then cast(date_format(stime,"yyyy-MM-dd 00:00:00") as timestamp) 
+else stime end as nstime,
+case when (maxetime = etime) then cast(date_format(etime,"yyyy-MM-dd 23:59:59") as timestamp) 
+else etime end as netime
+from openlab.stay_month sm
+left join (
+select uid, city, date, min(stime) as minstime, max(etime) as maxetime
+from openlab.stay_month
+where city in ('V0110000')
+group by uid, city, date
+) sud
+on sm.uid = sud.uid and sm.date = sud.date and sm.city = sud.city
+where sm.city in ('V0110000')
+) f
+where unix_timestamp(f.netime) - unix_timestamp(f.nstime) > 1800
+;
