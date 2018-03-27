3d7e31303370e667716aa787b1844f966ce850fa
進撃の雯雯老師 <askahu6@gmail.com>
2018-03-27 18:28:25 +0800

    Update 2_stay_month_longhu.sql

--- daas_longhu/2_stay_month_longhu.sql
+++ daas_longhu/2_stay_month_longhu.sql
@@ -1,5 +1,5 @@
 create table stay_month_longhu as
-select sm2.uid, sm2.city, sp2.lat, sp2.lon, sm2.nstime as stime, sm2.netime as etime
+select sm2.uid, sm2.city, sp2.lat, sp2.lon, sm2.date, sm2.nstime as stime, sm2.netime as etime
 from (
 select sm.uid, sm.city, sm.grid_id, sm.date, 
 case when (minstime = stime) then cast(date_format(stime,"yyyy-MM-dd 00:00:00") as timestamp) 
@@ -25,7 +25,7 @@ inner join map_longhu m
 on sp.city = m.city_code
 ) sp2
 on sm2.uid = sp2.uid and sm2.grid_id = sp2.grid_id and sm2.city = sp2.city
-group by sm2.uid, sm2.city, sp2.lat, sp2.lon, sm2.nstime, sm2.netime
+group by sm2.uid, sm2.city, sp2.lat, sp2.lon, sm2.date, sm2.nstime, sm2.netime
 ;
 
 select city, date, count(1) as n from stay_month_longhu group by city, date;
