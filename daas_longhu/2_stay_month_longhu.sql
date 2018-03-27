9f98e325c8577188d01d8a525e47585567341132
進撃の雯雯老師 <askahu6@gmail.com>
2018-03-27 18:04:25 +0800

    Create 2_stay_month_longhu.sql

---
+++ daas_longhu/2_stay_month_longhu.sql
@@ -0,0 +1,36 @@
+create table stay_month_longhu as
+select sm2.uid, sm2.city, sp2.lat, sp2.lon, sm2.nstime as stime, sm2.netime as etime
+from (
+select sm.uid, sm.city, sm.grid_id, sm.date, 
+case when (minstime = stime) then cast(date_format(stime,"yyyy-MM-dd 00:00:00") as timestamp) 
+else stime end as nstime,
+case when (maxetime = etime) then cast(date_format(etime,"yyyy-MM-dd 23:59:59") as timestamp) 
+else etime end as netime
+from stay_month sm
+inner join (
+select uid, city, date, min(stime) as minstime, max(etime) as maxetime
+from stay_month
+where unix_timestamp(etime) - unix_timestamp(stime) > 0
+group by uid, city, date
+) sud
+on sm.uid = sud.uid and sm.date = sud.date and sm.city = sud.city
+where unix_timestamp(sm.etime) - unix_timestamp(sm.stime) > 0
+) sm2
+inner join (
+select sp.city, sp.final_grid_id as grid_id, sp.uid, 
+floor((sp.weighted_centroid_lat-m.slat)/m.mlat) as lat,
+floor((sp.weighted_centroid_lon-m.slon)/m.mlon) as lon
+from stay_poi sp
+inner join map_longhu m
+on sp.city = m.city_code
+) sp2
+on sm2.uid = sp2.uid and sm2.grid_id = sp2.grid_id and sm2.city = sp2.city
+;
+drop table if exists time_map;
+create table time_map as
+select substr(cast(a.r as String),2,2) as stime, substr(cast(b.r as String),2,2) as etime 
+from 
+(select (row_number() over())+99 as r from stay_month limit 24) a, 
+(select (row_number() over())+99 as r from stay_month limit 24) b
+where a.r <= b.r
+;
