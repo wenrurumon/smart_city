3a1040d1571f31a7753ea8a5bab109c5c8d28d42
進撃の雯雯老師 <askahu6@gmail.com>
2018-07-03 17:59:38 +0800

    Create checkdata.sql

---
+++ daas2.0/checkdata.sql
@@ -0,0 +1,8 @@
+
+#sql type = 1;
+
+select sp.city, sp.date, m.city_name
+from (select city_code,city_name from ss_grid_wgs84 group by city_code,city_name) m 
+inner join (select city,date from stay_poi group by city,date) sp
+on m.city_code = sp.city
+group by sp.city, sp.date, m.city_name;
