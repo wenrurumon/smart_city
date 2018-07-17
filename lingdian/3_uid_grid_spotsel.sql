f082759488dbc7f330641f5b7525544ee0b0e3fe
進撃の雯雯老師 <askahu6@gmail.com>
2018-07-17 18:31:43 +0800

    Update 3_uid_grid_spotsel.sql

--- lingdian/3_uid_grid_spotsel.sql
+++ lingdian/3_uid_grid_spotsel.sql
@@ -1,16 +1,9 @@
 
 create table temp_ld20180717_table2 as
-select x.uid, x.gender, x.age, x.prov_id,
-min(x.stime) as stime, max(x.etime) as etime, sum(dtime) as dtime,
-x.spot0, x.spot from
-(
-select 
 uid, gender, age, prov_id, min(stime) as stime, gw, spot as spot1,
-cast((unix_timestamp(etime)-unix_timestamp(stime)) as bigint) as dtime,
+cast(sum(unix_timestamp(etime)-unix_timestamp(stime)) as bigint) as dtime,
 from temp_lc_pool_20180712
 where spot != 'NULL'
+group by uid, gender, age, prov_id, spot
 order by uid, stime
-) x
-group by x.uid, x.gender, x.age, x.prov_id, x.spot0, x.spot
-order by x.uid
 ;
