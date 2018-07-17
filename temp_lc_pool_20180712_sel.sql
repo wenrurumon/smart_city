02270a9111a553705074b4269499b21d365296dc
進撃の雯雯老師 <askahu6@gmail.com>
2018-07-17 14:42:49 +0800

    Update temp_lc_pool_20180712_sel.sql

--- temp_lc_pool_20180712_sel.sql
+++ temp_lc_pool_20180712_sel.sql
@@ -16,6 +16,25 @@ group by x.uid, x.gender, x.age, x.prov_id, x.spot0, x.spot
 order by x.uid
 ;
 
+create table temp_lc_pool_20180717_sel as
+select x.uid, x.gender, x.age, x.prov_id,
+min(x.stime) as stime, max(x.etime) as etime, sum(dtime) as dtime,
+x.spot0, x.spot1, x.spot2 from
+(
+select 
+uid, gender, age, prov_id, stime, etime, 
+cast((unix_timestamp(etime)-unix_timestamp(stime)) as bigint) as dtime,
+gw, spot as spot1, 
+lag(spot,1,spot) over(partition by uid order by stime) as spot0
+lead(spot,1,spot) over(partition by uid order by stime) as spot2
+from temp_lc_pool_20180712
+where spot != 'NULL'
+order by uid, stime
+) x
+group by x.uid, x.gender, x.age, x.prov_id, x.spot0, x.spot1, x.spot2
+order by x.uid
+;
+
 #景区信息
 select x.gender, x.age, x.prov_id, x.spot, count(1) as n, sum(x.dtime) as dtime
 from
@@ -28,7 +47,7 @@ group by x.gender, x.age, x.prov_id, x.spot
 ;
 
 #景区点到点
-select x.gender, x.age, x.prov_id, x.spot, count(1) as n, sum(x.dtime) as dtime
+select x.gender, x.age, x.prov_id, x.spot0, x.spot, count(1) as n, sum(x.dtime) as dtime
 from
 (
 select uid, gender, age, prov_id, sum(dtime) as dtime, spot0, spot
@@ -37,3 +56,16 @@ group by uid, gender, age, prov_id, spot0, spot
 ) x
 group by x.gender, x.age, x.prov_id, x.spot0, x.spot
 ;
+
+#景区点到点2
+select x.gender, x.age, x.prov_id, x.spot0, x.spot1, x.spot2, count(1) as n, sum(x.dtime) as dtime
+from
+(
+select uid, gender, age, prov_id, sum(dtime) as dtime, spot0, spot1, spot2
+from temp_lc_pool_20180717_sel
+group by uid, gender, age, prov_id, spot0, spot1, spot2
+) x
+group by x.gender, x.age, x.prov_id, x.spot0, x.spot1, x.spot2
+;
+
+
