6f78f87dcc7719c746e98ef2c011fd6217a60cf6
進撃の雯雯老師 <askahu6@gmail.com>
2018-07-17 19:09:11 +0800

    Update 4_path_len.sql

--- lingdian/4_path_len.sql
+++ lingdian/4_path_len.sql
@@ -1,5 +1,5 @@
 
-select a.prov_id, a.spot
+select a.prov_id, a.spot, count(1) as n, sum(r) as sr, max(r) as mr
 from 
-(select prov_id, uid, spot, count(1) from  temp_ld20180717_table2) a
+(select prov_id, uid, spot, count(1) as r from  temp_ld20180717_table2 group by prov_id, spot, uid) a
 group by a.prov_id, a.spot;
