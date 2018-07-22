14305ab5cd42ffcdc2c291e78711f04edcfbb5af
進撃の雯雯老師 <askahu6@gmail.com>
2018-07-22 23:25:20 +0800

    Update and rename r2_3_.sql to r2_3_prelim_output.sql

--- lingdian/r2_3_prelim_output.sql
+++ lingdian/r2_3_prelim_output.sql
@@ -23,7 +23,7 @@ select x.prov_id, x.gender, x.age, x.sumdate, count(1) as n from
 group by x1.uid, x1.prov_id, x1.gender, x1.age) x
 group by x.prov_id, x.gender, x.age, x.sumdate;
 
-#check r #5489
+#check r #5557
 select x.prov_id, x.r, count(1) as n from
 (select x1.uid, x1.prov_id, count(1) as r from
 (select uid, spot, prov_id, gender, age from temp_ldf0720_smsel3
@@ -32,16 +32,27 @@ group by spot, uid, prov_id, gender, age) x1
 group by x1.uid, x1.prov_id) x
 group by x.prov_id, x.r;
 
-#spots #5490
-select y.spot, x.r, count(1) as n from 
-(select uid, spot from temp_ldf0720_smsel3
+#check r #5560
+select x.prov_id, x.r, x.countdate, count(1) as n from
+(select x1.uid, x1.prov_id, count(distinct x1.date) as countdate, count(1) as r from
+(select uid, date, spot, prov_id, gender, age from temp_ldf0720_smsel3
 where spot is not null
-group by uid, spot) y
-inner join 
-(select x1.uid, x1.prov_id, count(1) as r from
-(select uid, spot, prov_id, gender, age from temp_ldf0720_smsel3
-where spot is not null
-group by spot, uid, prov_id, gender, age) x1
+group by spot, date, uid, prov_id, gender, age) x1
 group by x1.uid, x1.prov_id) x
-on x.uid = y.uid
-group by y.spot, x.r;
+group by x.prov_id, x.r, x.countdate;
+
+#spots #5561
+select x2.prov_id, x2.n, count(1) as n2 from 
+(
+select x1.uid, x1.date, count(1) as n, x1.prov_id from
+(select uid, date, spot, prov_id from temp_ldf0720_smsel3
+where spot is not null
+group by spot, date, uid, prov_id) x1
+group by x1.uid, x1.date, x1.prov_id
+) x2
+group by x2.prov_id, x2.n;
+
+#
+
+
+
