8cf88e3f3cd87dca8fa4850649a5cca3ce0ae368
進撃の雯雯老師 <askahu6@gmail.com>
2018-07-22 21:42:04 +0800

    Update r2_3_.sql

--- lingdian/r2_3_.sql
+++ lingdian/r2_3_.sql
@@ -2,32 +2,24 @@
 #staymonth中的条目筛选
 #fx.date, fx.uid, fx.gender, fx.age, fx.prov_id, fx.ptype, fx.stime, fx.etime, fx.gw, fx.lat, fx.lon, fx.spot
 
+#prov, profile (sumdate) count #5555
 select x2.prov_id, x2.sumdate, count(1) as n from 
 (select x.uid, x.prov_id, sum(x.date) as sumdate from 
 (select uid, date, prov_id from temp_ldf0720_smsel3 group by uid, date, prov_id) x
 group by x.uid, x.prov_id) x2
 group by x2.prov_id, x2.sumdate;
 
-#prov, spot, profile count #5483
+#prov, spot, profile (gender, age, sumdate) count #5554
 select x.prov_id, x.gender, x.age, x.spot, x.sumdate, count(1) as n from
 (select x1.uid, x1.prov_id, x1.gender, x1.age, x1.spot, sum(x1.date) as sumdate from
 (select uid, date, prov_id, gender, age, spot from temp_ldf0720_smsel3 group by date, uid, prov_id, gender, age, spot) x1
 group by x1.uid, x1.prov_id, x1.gender, x1.age, x1.spot) x
 group by x.prov_id, x.gender, x.age, x.spot, x.sumdate;
 
-#prov, profile count #5486
+#prov, profile count #5556
 select x.prov_id, x.gender, x.age, x.sumdate, count(1) as n from
 (select x1.uid, x1.prov_id, x1.gender, x1.age, sum(x1.date) as sumdate from
 (select uid, date, prov_id, gender, age from temp_ldf0720_smsel3 group by date, uid, prov_id, gender, age) x1
-group by x1.uid, x1.prov_id, x1.gender, x1.age) x
-group by x.prov_id, x.gender, x.age, x.sumdate;
-
-#prov, profile where spot is true #
-select x.prov_id, x.gender, x.age, x.sumdate, count(1) as n from
-(select x1.uid, x1.prov_id, x1.gender, x1.age, sum(x1.date) as sumdate from
-(select uid, date, prov_id, gender, age from temp_ldf0720_smsel3
-where spot is not null
-group by date, uid, prov_id, gender, age) x1
 group by x1.uid, x1.prov_id, x1.gender, x1.age) x
 group by x.prov_id, x.gender, x.age, x.sumdate;
 
