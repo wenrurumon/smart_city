f5f60b325cb9b04f6b061a290533ab1c6f78978b
進撃の雯雯老師 <askahu6@gmail.com>
2018-07-23 18:21:52 +0800

    Update r2_4_moveline.sql

--- lingdian/r2_4_moveline.sql
+++ lingdian/r2_4_moveline.sql
@@ -1,17 +1,17 @@
 
 #uid, date, stime, cspot
 
-select y3.prov_id, x2.spot1, x2.spot2, x2.spot3, x2.spot4, x2.spot5, x2.spot6, y3.cspot, count(1) as n from 
+select y3.prov_id, x2.spot1, x2.spot2, x2.spot3, x2.spot4, x2.spot5, x2.spot6, y3.cspot, sum(y3.gw) as w, count(1) as n from 
 (
-	select y2.prov_id, y2.uid, y2.date, y2.stime, y2.cspot
+	select y2.prov_id, y2.uid, y2.date, y2.stime, y2.cspot, y2.gw
 	from(
 		select y1.prov_id, y1.uid, y1.date, min(y1.stime) as stime, count(distinct spot) as cspot
 		from (
-			select uid, date, spot, prov_id, min(stime) as stime, max(etime) as etime from temp_ldf0720_smsel3
+			select uid, date, spot, prov_id, gw, min(stime) as stime, max(etime) as etime from temp_ldf0720_smsel3
 			where spot is not null
-			group by uid, date, spot, prov_id
+			group by uid, date, spot, prov_id, gw
 		) y1
-		group by y1.uid,y1.date,y1.prov_id
+		group by y1.uid,y1.date,y1.prov_id, y1.gw
 	) y2
 	where y2.cspot > 1
 	group by y2.prov_id, y2.uid, y2.date, y2.stime, y2.cspot
