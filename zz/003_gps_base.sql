b3e41671669d72691b13535a37f4dec7079e0aab
進撃の雯雯老師 <askahu6@gmail.com>
2018-08-13 13:50:59 +0800

    Create 003_gps_base.sql

---
+++ zz/003_gps_base.sql
@@ -0,0 +1,5 @@
+select a.city_code, b.area_desc, b.prov_name, min(a.ext_min_x) as slon, min(a.ext_min_y) as slat,
+avg(a.ext_max_x-ext_min_x) as mlon, avg(a.ext_max_y-ext_min_y) as mlat
+from ss_grid_wgs84 a inner join area_code b
+on a.city_code = b.area_code
+group by a.city_code, b.area_desc, b.prov_name;
